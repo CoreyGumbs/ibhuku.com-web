59cbf4aee7267786b1a326bbbaaf8b4c945d0e71
Corey Gumbs <coreygumbs@gmail.com>
2016-12-16 18:40:01 -0500

    Account App: Added modelForm, updated User CreateView, made changes to form and base templates, added Bootstrap

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,1 +1,12 @@
 from django import forms
+from django.forms import ModelForm
+from django.utils.translation import ugettext_lazy as _
+
+from .models import IbkUser
+
+
+class IbkUserSignUpForm(ModelForm):
+	password2 = forms.CharField(label=_('Confirm Password'), widget=forms.PasswordInput())
+	class Meta:
+		model = IbkUser
+		fields = ('first_name', 'last_name', 'email', 'password', 'password2')
--- accounts/views.py
+++ accounts/views.py
@@ -5,12 +5,12 @@ from django.views.generic.base import TemplateView
 from django.views.generic.edit import CreateView
 
 from .models import IbkUser, Profile
+from .forms	 import IbkUserSignUpForm
 
 # Create your views here.
 class AccountsIndex(TemplateView):
 	template_name = 'accounts/base.html'
 
 class AccountSignUp(CreateView):
-	model = IbkUser
-	fields = ('first_name', 'last_name', 'email', 'password')
+	form_class = IbkUserSignUpForm
 	template_name = 'accounts/signup_form.html'

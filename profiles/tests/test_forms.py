d44e7a25f4cd441edcd6790546305bc74ba68834
Corey Gumbs <coreygumbs@gmail.com>
2017-02-07 14:35:00 -0500

    [tests] Added Form tests for ProfileAvatarUploadViewForm

--- profiles/tests/test_forms.py
+++ profiles/tests/test_forms.py
@@ -17,11 +17,25 @@ class TestDataFixture(TestCase):
     @classmethod 
     def setUpTestData(cls):
         cls.user = IbkUser.objects.create_user(email = "mctest@test.com", name="McTestMcTesty", username="McTestyRocks", password="password12345")
+        cls.data = {
+            'name': cls.user.name,
+        }
 
 class TestProfileAvatarUploadForm(TestDataFixture):
     """
     Test Profile avatar upload form.
     """
 
-    def test_profile_avatar_upload_form_is_bound(self):
+    def test_profile_avatar_upload_form_is_not_bound(self):
         self.assertEqual(self.av_upload.is_bound, False)
+
+    def test_profile_avatar_upload_form_is_bound(self):
+        self.av_upload = ProfileAvatarUploadForm(data={})
+        self.assertEqual(self.av_upload.is_bound, True)
+
+    def test_profile_avatar_upload_form_is_valid(self):
+        self.av_upload = ProfileAvatarUploadForm(data=self.data)
+        self.assertIs(self.av_upload.is_valid(), True)
+
+    def test_profile_avatar_upload_from_is_not_valid(self):
+        self.assertIs(self.av_upload.is_valid(), False)
